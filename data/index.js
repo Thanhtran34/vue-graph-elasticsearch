e61f516b85ee7cfaf6e8185ccb57a4926a030389
Thanh Tran <tt222kj@student.lnu.se>
2022-04-16 21:15:07 +0200

    convert data

---
+++ data/index.js
@@ -0,0 +1,14 @@
+// Convert csv file to json file
+import csv from "csvtojson";
+import fs from "fs";
+
+const csvFilePath = "./population.csv"
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{
+    fs.writeFileSync("../src/data/output.json", JSON.stringify(jsonObj), "utf-8", (err) => {
+      if(err) {
+        console.log(err)
+      }
+    })
+})
