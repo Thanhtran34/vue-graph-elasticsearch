a01e20612b2de80f712551636063c66fd1d60125
Thanh Tran <tt222kj@student.lnu.se>
2022-04-22 18:26:04 +0200

    deploying

--- src/elastic/connection.js
+++ src/elastic/connection.js
@@ -1,4 +1,11 @@
 // Connect to elasticsearch
 import { Client } from "@elastic/elasticsearch";
 
-export const client = new Client({ node: `${process.env.BASE_URL}` });
+export const client = new Client({
+  node: `${process.env.BASE_URL}`,
+
+  auth: {
+    username: "elastic",
+    password: "YlaemlrrI+EwXn9h",
+  },
+});
