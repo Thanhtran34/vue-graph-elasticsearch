46a4e363c48149223e68ff1fa9eeefab4bd3b841
Thanh Tran <tt222kj@student.lnu.se>
2022-04-22 17:20:06 +0200

    add pie chart

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
-    <h1>Data visualization</h1>
-    <title>World Population</title>
+    <h1>World Population visualization</h1>
+    <p>
+      <i
+        >(Population by country from 1950-2022. Data shows the change in shape
+        of population of country based on age in seven decades. Age Range: 0 -100+)</i
+      >
+    </p>
+    <p><i><strong>Bar Chart</strong> shows the total population. <strong>Pie Chart</strong> shows the percentage of Female in each age group.</i></p>
+    <br />
+    <title>Data visualizasion</title>
     <form @submit.prevent="search('country', 'year')">
       <select v-model="selectedCountry">
         <option>Choose Country</option>
@@ -13,7 +21,6 @@
           {{ data.country }}
         </option>
       </select>
-      <br />
       <select v-model="selectedYear">
         <option>Choose Year</option>
         <option
@@ -24,13 +31,17 @@
           {{ data.year }}
         </option>
       </select>
-      <br />
+      <p>
+        <a href="https://www.populationpyramid.net/" target="_blank"
+          >Source From Kaggle</a
+        >
+      </p>
       <button @click="search('country', 'year')" class="btn btn-success">
         SEARCH
       </button>
     </form>
     <br />
-    <BarChart :axisData="axisData"/>
+    <BarChart :axisData="axisData" v-if="axisData.length > 0" />
   </div>
 </template>
 
@@ -42,7 +53,7 @@ import BarChart from "./components/BarChart.vue";
 export default {
   name: "App",
   components: {
-    BarChart,
+    BarChart
   },
   data() {
     return {
@@ -50,7 +61,9 @@ export default {
       years: Data.getListOfYears(),
       selectedCountry: "Choose Country",
       selectedYear: "Choose Year",
+      selectedDiagram: "Choose Diagram",
       axisData: [],
+      diagrams: [{ diagram: "Bar Chart" }, { diagram: "Pie Chart" }],
     };
   },
   methods: {
@@ -58,11 +71,16 @@ export default {
       country = this.selectedCountry;
       year = this.selectedYear;
       axios
-        .get(`http://localhost:8080/search-country/${country}/${year}`)
+        .get(`${process.env.VUE_APP_URL}/search-country/${country}/${year}`)
         .then((response) => {
           this.axisData = response.data.records
-                               .filter(el => el.Country === this.selectedCountry)
-                               .map(el => ({x: el.Age, y: parseInt(el.Male), z: parseInt(el.Female), country: el.Country}));
+            .filter((el) => el.Country === this.selectedCountry)
+            .map((el) => ({
+              x: el.Age,
+              y: parseInt(el.Male),
+              z: parseInt(el.Female),
+              country: el.Country,
+            }));
         })
         .catch(function (error) {
           console.log(error);
@@ -79,10 +97,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
+}
+
+body {
+  background-color: rgb(229, 221, 221);
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
 }
 
 select {
+  margin-left: 10px;
   margin-bottom: 20px;
   width: auto;
   font-size: 1.1rem;
@@ -96,9 +123,9 @@ button {
   background-color: rgb(113, 160, 190);
 }
 
-h1{
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  font-size: 3rem;
-  color: rgb(132, 187, 221);
+h1 {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 2rem;
+  color: rgb(5, 74, 116);
 }
 </style>
--- client/src/components/BarChart.vue
+++ client/src/components/BarChart.vue
@@ -2,7 +2,8 @@
   <div>
     <!-- this part is needed to load data for bar chart faster-->
     <p style="display: none">{{ axisData }}</p>
-    <svg></svg>
+    <svg id="svg1"></svg>
+    <svg id="svg2"></svg>
   </div>
 </template>
 
@@ -20,12 +21,12 @@ export default {
     },
   },
   methods: {
-    renderChart() {
-      const height = 400;
+    renderBarChart() {
+      const height = 370;
       const roundedHeight = Math.ceil((height + 1) / 10) * 10;
-      const width = 800;
+      const width = 550;
       let color = d3.scaleOrdinal(d3.schemeCategory10);
-    
+
       // set the ranges
       const xScale = d3
         .scaleBand()
@@ -39,7 +40,7 @@ export default {
         .range([roundedHeight, 10]);
 
       const container = d3
-        .select("svg")
+        .select("#svg1")
         .classed("chart-container", true)
         .style("height", `${roundedHeight}px`)
         .style("width", `${width}px`);
@@ -55,11 +56,12 @@ export default {
         .attr("height", (data) => roundedHeight - yScale(data.y + data.z))
         .attr("x", (data) => xScale(data.x))
         .attr("y", (data) => yScale(data.y + data.z))
-        .attr("fill", d=>color(d));
+        .attr("fill", (d) => color(d))
       // add the x Axis
       container
         .append("g")
         .attr("transform", "translate(0," + roundedHeight + ")")
+        .style("font", "16px times")
         .call(d3.axisBottom(xScale))
         .selectAll("text")
         .attr("transform", "translate(-15, 15) rotate(-45)");
@@ -67,17 +69,133 @@ export default {
       // add the y Axis
       container
         .append("g")
-        .call(d3.axisLeft(yScale));
+        .style("font", "16px times")
+        .call(d3.axisLeft(yScale));    
+    },
+
+    renderPieChart() {
+      this.buildChart(this.filteredData, {
+        name: (d) => 'Female: ' + d.x,
+        value: (d) => (d.z/(d.y + d.z) * 100).toFixed(2),
+        width: 550,
+        height: 380,
+      });
+    },
+    buildChart(
+      data,
+      {
+        name = ([x]) => x, // given d in data, returns the (ordinal) label
+        value = ([, y]) => y, // given d in data, returns the (quantitative) value
+        title, // given d in data, returns the title text
+        width = 350, // outer width, in pixels
+        height = 300, // outer height, in pixels
+        innerRadius = 0, // inner radius of pie, in pixels (non-zero for donut)
+        outerRadius = Math.min(width, height) / 2, // outer radius of pie, in pixels
+        labelRadius = innerRadius * 0.2 + outerRadius * 0.8, // center radius of labels
+        format = ",", // a format specifier for values (in the label)
+        names, // array of names (the domain of the color scale)
+        colors, // array of colors for names
+        stroke = innerRadius > 0 ? "none" : "white", // stroke separating widths
+        strokeWidth = 1, // width of stroke separating wedges
+        strokeLinejoin = "round", // line join of stroke separating wedges
+        padAngle = stroke === "none" ? 1 / outerRadius : 0, // angular separation between wedges
+      } = {}
+    ) {
+      // Compute values.
+      const N = d3.map(data, name);
+      const V = d3.map(data, value);
+      const I = d3.range(N.length).filter((i) => !isNaN(V[i]));
+
+      // Unique the names.
+      if (names === undefined) names = N;
+      names = new d3.InternSet(names);
+
+      // Chose a default color scheme based on cardinality.
+      if (colors === undefined) colors = d3.schemeSpectral[names.size];
+      if (colors === undefined)
+        colors = d3.quantize(
+          (t) => d3.interpolateSpectral(t * 0.8 + 0.1),
+          names.size
+        );
+
+      // Construct scales.
+      const color = d3.scaleOrdinal(names, colors);
+
+      // Compute titles.
+      if (title === undefined) {
+        const formatValue = d3.format(format);
+        title = (i) => `${N[i]}\n${formatValue(V[i])}` + '%';
+      } else {
+        const O = d3.map(data, (d) => d);
+        const T = title;
+        title = (i) => T(O[i], i, data);
+      }
+
+      // Construct arcs.
+      const arcs = d3
+        .pie()
+        .padAngle(padAngle)
+        .sort(null)
+        .value((i) => V[i])(I);
+      const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+      const arcLabel = d3
+        .arc()
+        .innerRadius(labelRadius)
+        .outerRadius(labelRadius);
+
+      const svg = d3
+        .select("#svg2")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .attr("style", "max-width: 40%; height: auto; height: intrinsic;");
+
+      svg
+        .append("g")
+        .attr("stroke", stroke)
+        .attr("stroke-width", strokeWidth)
+        .attr("stroke-linejoin", strokeLinejoin)
+        .selectAll("path")
+        .data(arcs)
+        .join("path")
+        .attr("fill", (d) => color(N[d.data]))
+        .attr("d", arc)
+        .append("title")
+        .text((d) => title(d.data));
+
+      svg
+        .append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .attr("text-anchor", "middle")
+        .selectAll("text")
+        .data(arcs)
+        .join("text")
+        .attr("transform", (d) => `translate(${arcLabel.centroid(d)})`)
+        .selectAll("tspan")
+        .data((d) => {
+          const lines = `${title(d.data)}`.split(/\n/);
+          return d.endAngle - d.startAngle > 0.25 ? lines : lines.slice(0, 1);
+        })
+        .join("tspan")
+        .attr("x", 0)
+        .attr("y", (_, i) => `${i * 1.1}em`)
+        .attr("font-weight", (_, i) => (i ? null : "bold"))
+        .text((d) => d);
+
+      return Object.assign(svg.node(), { scales: { color } });
     },
   },
   mounted() {
-    this.renderChart();
+    this.renderBarChart();
+    this.renderPieChart();
   },
   updated() {
-    this.renderChart();
+    this.renderBarChart();
+    this.renderPieChart();
   },
   beforeUpdate() {
-    var svg = d3.select("svg");
+    const svg = d3.select("svg");
     svg.selectAll("*").remove();
   },
 };
@@ -85,9 +203,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-.chart-container {
-  border: 1px solid navy;
-  margin-left: auto;
+.chart-container, #svg2 {
+  border: 2px solid navy;
+  margin-top: 1em;
+  margin-left: 20px;
   margin-right: auto;
   overflow: visible;
 }
